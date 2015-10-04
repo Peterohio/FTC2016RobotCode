acf1e3485efa8051fc58451ae9e9f7c236379605
AndrewH48 <androidholaday@gmail.com>
2015-10-04 15:59:48 -0400

    mde attachments interface

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestTank.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Dublin FTC on 9/15/2015.
+ */
+public class TestTank extends LinearOpMode
+{
+    public void runOpMode () throws InterruptedException
+    {
+
+    }
+}

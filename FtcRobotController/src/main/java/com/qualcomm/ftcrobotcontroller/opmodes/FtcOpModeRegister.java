40612dff1b5af8ff7a64244a5eb0fed6a6315c00
PeterG <peterohio1@gmail.com>
2015-10-15 20:27:55 -0400

    Added to the code I last made to move robot out of the way for the other team (pseudo code)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,7 +55,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
     manager.register("TankTeleopTest", TankTeleopTest.class);
-
+    manager.register("TestBot_PG" , TestBot_PG.class);
     /*
      * The following op modes are example op modes provided by QualComm.
      * Uncomment the lines to make the op modes available to the driver station.
